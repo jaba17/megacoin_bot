a077517e07c75cca6c605c0a9d5c9a0e415b9821
jaba17 <Jakob.bach15@gmail.com>
2018-02-13 14:25:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ client.on('message', message => {
         
     	message.channel.send('You participated successfull in this giveaway!');
         participantsNumber = participantsNumber+1;
-        message.channel.send(participantsNumber+'Participants');
+        message.channel.send(participantsNumber+' participants');
 
         
   	}
