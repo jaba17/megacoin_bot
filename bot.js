1a434b714ed1191ef95cd0c6ce5f2145de6993c6
jaba17 <Jakob.bach15@gmail.com>
2018-03-04 21:58:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('message', message => {
 client.on('message', message => {
 
     if (message.content == 'discordapp.com/invite/' || message.content == 'discord.gg/') {
-        message.purge(1);
+        message.delete(1);
         if(Math.random() > 0.5){
             message.channel.send("I didn't see that one :blush:");
         }else{
