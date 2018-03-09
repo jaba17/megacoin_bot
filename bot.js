1fa10bff7d6000c46b437f2e09ea7e9bee3c4460
jaba17 <Jakob.bach15@gmail.com>
2018-03-09 20:40:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+/*const Discord = require('discord.js');
 const client = new Discord.Client();
 
 //var participantsNumber = 0;
@@ -51,7 +51,7 @@ client.on('message', message => {
 	}
 });
 */
-client.on('message', message => {
+/*client.on('message', message => {
 
     if (message.content === 'ping' || message.content === "Ping" || message.content === "PING") {
 
@@ -100,3 +100,4 @@ client.on('message', message => {
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);
+*/
